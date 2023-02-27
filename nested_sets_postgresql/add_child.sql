41e0ae4eabfaa11dda4176fd7f1cb18ff11c6b41
MohammedAl-rowad <rowadwo@gmail.com>
2023-02-27 01:18:26 +0100

    filter by tree_id

--- nested_sets_postgresql/add_child.sql
+++ nested_sets_postgresql/add_child.sql
@@ -10,15 +10,15 @@ language plpgsql
 as $$
 DECLARE 
     parent_left NUMERIC;
-    tree_id VARCHAR(255);
+    tree_id_ VARCHAR(255);
 begin
     SELECT "left", nodes.tree_id 
-    INTO parent_left, tree_id FROM nodes WHERE "id" = perent_id;
+    INTO parent_left, tree_id_ FROM nodes WHERE "id" = perent_id;
 
-    UPDATE nodes SET "right" = "right" + 2 WHERE "right" > parent_left;
-    UPDATE nodes SET "left" = "left" + 2 WHERE "left" > parent_left;
+    UPDATE nodes SET "right" = "right" + 2 WHERE "right" > parent_left AND nodes.tree_id = tree_id_;;
+    UPDATE nodes SET "left" = "left" + 2 WHERE "left" > parent_left AND nodes.tree_id = tree_id_;;
     
     INSERT INTO nodes("name", "left", "right", "tree_id")
-    VALUES (node_name, parent_left + 1, parent_left + 2, tree_id);
+    VALUES (node_name, parent_left + 1, parent_left + 2, tree_id_);
     commit;
 end;$$
