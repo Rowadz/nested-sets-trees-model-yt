f5d55062f650222a3331989554749146e8481aa2
MohammedAl-rowad <rowadwo@gmail.com>
2023-03-03 22:57:58 +0100

    add create_example_tree

---
+++ nested_sets_postgresql/create_example_tree.sql
@@ -0,0 +1,10 @@
+CALL insert_tree_root('ROWADZ');
+
+CALL add_child(1, 'A');
+CALL add_child(1, 'B');
+
+CALL add_child(2, 'F');
+
+CALL add_child(3, 'C');
+CALL add_child(3, 'D');
+CALL add_child(3, 'E');
